1e2895ba7fa522b699544f77905fa012282b8591
qianxuebing <2625318262@qq.com>
2017-11-11 10:41:17 +0800

    添加js

--- js/index.js
+++ js/index.js
@@ -1,80 +1,108 @@
-$(document).ready(function(){
-	// subnav
-	$(".sidebar ul li").hover(function(){
-				var index = $(this).index();
-				$(".sidebar .subnav .item").removeClass("active").eq(index).addClass("active");
-			
-			});
-	
+$(document).ready(function() {
+	//subnav
+	$(".sidebar ul li").hover(function() {
+		var index = $(this).index();
+		$(".sidebar .subnav .item").removeClass("active").eq(index).addClass("active");
 
-	/***************banner carousel(轮播) start************/
+	});
+
+	//banner carousel(轮播) start
 	var ul = $(".banner_img ul");
 	var count = $(".banner_img ul").children().length;
 	var width = $(".banner_img").width();
 	var index = 0;
 	var items = $(".banner_img .items").children();
-	ul.width(width*(count+1));
-	
+	ul.width(width * (count + 1));
 	var first = $(".banner_img ul li").first().clone(true);
 	ul.append(first);
-	
 	items.eq(0).addClass("current");
-	
-	var t = setInterval(run,2000);
-	
-	$(".banner_img").hover(function(){
+	var t = setInterval(run, 2000);
+	$(".banner_img").hover(function() {
 		clearInterval(t);
-	},function(){
-		t = setInterval(run,2000);
+	}, function() {
+		t = setInterval(run, 2000);
+	});
+	$(".banner_img .items span").mouseover(function() {
+		var index = $(this).index();
+		$(".banner_img .items span").removeClass("current").eq(index).addClass("current");
+		ul.css("left", -1 * width * index + "px");
 	});
 
-	function run(){
-		index = (index+1)%count;
+	function run() {
+		index = (index + 1) % count;
 		items.removeClass("current").eq(index).addClass("current");
-		if (index == 0) {			
-			ul.animate({left:-1*width*count+"px"},500,function(){
-				ul.css("left","0px");
-		});
-		} else{		
-			ul.animate({left:-1*width*index+"px"},500);
-		}		
+		if(index == 0) {
+			ul.animate({
+				left: -1 * width * count + "px"
+			}, 500, function() {
+				ul.css("left", "0px");
+			});
+		} else {
+			ul.animate({
+				left: -1 * width * index + "px"
+			}, 500);
+		}
 	}
-	/*************banner end*****************/
-	
-	/***************new product start************************/	
+	//banner end
+
+	//new product start
 	var new_ul = $(".product_img ul");
 	var new_count = $(".product_img ul").children().length;
 	var new_width = $(".product_img").width();
-	var d = 0;
-	var points = $(".product_img .items");
-	
-	
-	new_ul.width(new_width*(new_count+1));
-	
+	var point = $(".product_img .items");
+
+	new_ul.width(new_width * (new_count + 1));
 	var new_first = $(".product_img ul li").first().clone(true);
 	new_ul.append(new_first);
+	for(var i = 0; i < new_count; i++) {
+		point.append("<span></span>");
+	}
+	point.css("margin-left", -1 * point.width() / 2 + "px");
+	var item = point.children();
+	item.eq(0).addClass("current");
+	item.mouseover(function(){
+		var d = $(this).index();
+		console.log(d);
+		item.removeClass("current").eq(d).addClass("current");
+		new_ul.css("left",-1*new_width*d+"px");
+	});
+	//new product end
 	
-	for (var i = 0; i < new_count; i++) {
-		points.append("<span></span>");
+	//兑换专区  start
+	var n = 0;
+	var count1 = $(".product .productcon dl").length;
+	var width1 = $(".product .productcon dl").width();	
+	var T = setInterval(running,2000);
+	$(".product").hover(function(){
+		clearInterval(T);
+		$('.clickyou').show();
+		$('.clickzuo').show();
+	},function(){
+		$('.clickyou').hide();
+		$('.clickzuo').hide();
+		T = setInterval(running,2000);
+	});
+	$(".clickyou").click(function(){
+		n = (n+1)%count;
+		$(".productcon").animate({left:-1*width*count+"px"},500,function(){
+				$(".productcon").css("left","0px");
+			});
+	});
+	$(".clickzuo").click(function(){
+		n = (n-1+count)%count;
+		$(".productcon").animate({left:-1*width*count+"px"},500)
+	});
+	function running(){
+		n = (n+1)%count;
+		if (n == 0) {
+			$(".productcon").animate({left:-1*width*count+"px"},500,function(){
+				$(".productcon").css("left","0px");
+			});
+		} else{
+			$(".productcon").animate({left:-1*width*n+"px"},500);
+		}
 	}
 	
-	points.css("margin-left",-1*points.width()/2+"px");
-	
-	var item = points.children();
-	item.eq(0).addClass("current");
-	
-	setInterval(function(){
-		d = (d+1)%new_count;
-		item.removeClass("current").eq(d).addClass("current");
-		if (d == 0) {			
-			new_ul.animate({left:-1*new_width*new_count+"px"},500,function(){
-				new_ul.css("left","0px");
-			});
-		} else{		
-			new_ul.animate({left:-1*new_width*d+"px"},500);
-		}	
-	},2000);
-	/***************new product end************************/
 	
 	
 	
@@ -82,6 +110,5 @@ $(document).ready(function(){
 	
 	
 	
-	
-	
+
 })
